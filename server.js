6edff96c184becb338ab30a3acc87f2cecd8ff5e
Dancairo-Gomes <76046040+Dancairo-Gomes@users.noreply.github.com>
2021-07-29 21:16:56 -0300

    Update server.js

--- server.js
+++ server.js
@@ -1,19 +1,25 @@
 var express = require('express');
 var app = express();
-app.use(express.static(__dirname));
+
 var bodyParser = require("body-parser")
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: false}))
+
 const cors = require("cors");
+app.use(cors());
+
 app.use(express.static('public'));
 app.set('view engine', 'ejs');
-app.use(cors());
+
+var socket = require('socket.io')
+
 var mongoose = require('mongoose');
 var dbUrl = 'mongodb+srv://Dancairo:dan19091993@cluster0.ahsnl.mongodb.net/myFirstDatabase?retryWrites=true&w=majority';
 
 mongoose.connect(dbUrl , (err) => { 
     console.log("mongodb connected",err);
  })
+
 
 var Message = mongoose.model("Message",{ name : String, message : String})
 
@@ -27,16 +33,21 @@ app.get('/messages', (req, res) => {
     })
   })
 
-
-app.post('/messages', (req, res) => {
+  app.post('/messages', (req, res) => {
     var message = new Message(req.body);
     message.save((err) =>{
       if(err)
         sendStatus(500);
+      io.emit('message', req.body);
       res.sendStatus(200);
     })
   })
 
-var server = app.listen(3000, () => {
-    console.log('Server is running on port', server.address().port);
-})
+  var server = app.listen(3000, function(){
+    console.log('listening for requests on port 3000,');
+});
+
+let io = socket(server)
+io.on('connection', function(socket){
+  console.log(`${socket.id} is connected`);
+});
